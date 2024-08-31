455783f9f5df4290ec2c2c47d51f0d7802ab48db
ansh2250 <anshsinha947@gmail.com>
2024-08-31 16:02:27 +0530

    commited before dipolyement

--- script.js
+++ script.js
@@ -39,3 +39,41 @@ function sendMessage() {
   messages.innerHTML += botMessage;
   document.getElementById('userInput').value = '';
 }
+
+
+
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".wrapper",
+        start: "top top",
+        end: "+=150%",
+        pin: true,
+        scrub: true,
+        markers: true
+      }
+    })
+    .to("img", {
+      scale: 2,
+      z: 350,
+      transformOrigin: "center center",
+      ease: "power1.inOut"
+    })
+    .to(
+      ".section.hero",
+      {
+        scale: 1.1,
+        transformOrigin: "center center",
+        ease: "power1.inOut"
+      },
+      "<"
+    );
+});
+
+// service page
+
